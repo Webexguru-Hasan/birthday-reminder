8c04988fed26bd8ff12011b4367b207d0d50b144
Hasan Wazid <Webexguruhasan95@gmail.com>
2023-06-04 21:07:56 -0700

    [CHANGED] the name of Data

--- src/Component/BirthData.js
+++ src/Component/BirthData.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 1,
-    name: "Hasan wazid",
+    name: "JS Josim",
     age: 27,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
