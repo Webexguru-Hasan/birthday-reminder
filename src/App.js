cfec0b8c0dad586bdfe09b79d45cb5c1a117d90a
Hasan Wazid <Webexguruhasan95@gmail.com>
2023-06-04 20:10:27 -0700

    [COMPLETED] Birthday reminder project all functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-
-import { useState } from 'react';
-import './App.css';
-import List from './Components/List';
-import Data from './Components/Data';
+import { useState } from "react";
+import "./App.css";
+import BirthData from "./Component/BirthData";
+import BirthList from "./Component/BirthList";
 
 function App() {
-  const [peoples, setPeoples] = useState(Data)
+  const [peoples, setPeoples] = useState(BirthData);
   return (
-   <>
-   <main>
-    <section className="container">
-      <h3>{peoples.length} birthdays today</h3>
-      <List peoples={peoples} />
-      <button onClick={() => setPeoples([])}>clear all</button>
-    </section>
-   </main>
-   </>
+    <>
+      <main>
+        <section className="container">
+          <h3>{peoples.length} birthdays today</h3>
+          <BirthList peoples={peoples} />
+          <button onClick={() => setPeoples([])}>clear all</button>
+        </section>
+      </main>
+    </>
   );
 }
 
